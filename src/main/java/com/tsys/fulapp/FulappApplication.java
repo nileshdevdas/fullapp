4b53586d186ab7a9bfc1d20ba0257c8db6cb018e
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2018-10-17 14:49:53 +0530

    Initial Commit

---
+++ src/main/java/com/tsys/fulapp/FulappApplication.java
@@ -0,0 +1,12 @@
+package com.tsys.fulapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulappApplication.class, args);
+	}
+}
